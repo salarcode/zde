f3ec84631d0c7bfbca7290e4e72e533d9f08c00c
salarcode <salar2k@gmail.com>
2020-08-24 14:55:12 +1000

    build

---
+++ build/precache-manifest.9201adf3acab94df213ee5ff8498e331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38263d18bbd060f76f358c13a71931eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "563f86c00a904a5f106e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b35b6580010e39ba1dd4",
+    "url": "/static/js/2.b61a8073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b61a8073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563f86c00a904a5f106e",
+    "url": "/static/js/main.d38484db.chunk.js"
+  },
+  {
+    "revision": "47e93e39dfd2546d12ee",
+    "url": "/static/js/runtime-main.22249d3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
